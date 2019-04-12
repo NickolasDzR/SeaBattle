8b1e1c84302df0b0618867ce6670807df9d9e682
NickolasDzR <42074447+NickolasDzR@users.noreply.github.com>
2019-04-12 15:58:08 +0300

    commit

--- battleship.js
+++ battleship.js
@@ -14,6 +14,9 @@ while (isSunk == false) {
     guesses = guesses + 1;
     if (guess == location1 || guess == location2 || guess == location3) {
       hits = hits + 1;
+      alert("you're git 'em, HIT!")
+    } else {
+      alert("MISS:(");
     }
     if (hits == 3) {
       isSunk = true;
