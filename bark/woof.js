505c9acdde01db160a1124ebb76fd248b22f2c24
NickolasDzR <42074447+NickolasDzR@users.noreply.github.com>
2019-04-13 16:48:23 +0300

    commit

---
+++ bark/woof.js
@@ -0,0 +1,7 @@
+function bark(name, weight) {
+  if (weight > 20) {
+    console.log(name + " WOOF WOOF");
+  } else {
+    console.log(name + " woof woof");
+  }
+}
---
+++ sweater.js
@@ -0,0 +1,8 @@
+function temperature(temp, whatShallWear) {
+  if (temp < 60) {
+    console.log("wear a jacket");
+  } else {
+    console.log("wear a t-shirt");
+   } else if (temp < 70);
+    console.log("wear a sweater");
+  }
