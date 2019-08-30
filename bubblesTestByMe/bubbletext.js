ccc0f272adf83fe054db9172003bcc54bb2997d1
Nickolas <nikolyae2019@gmail.com>
2019-08-30 09:19:41 +0300

    task bb

--- bubblesTestByMe/bubbletext.js
+++ bubblesTestByMe/bubbletext.js
@@ -6,28 +6,55 @@ var scores = [60, 50, 60, 58, 54, 54,
     41, 53, 55, 61, 51, 44
 ];
 
-var bubblesTest;
-var highestScore = 0;
-var mostBiggest = [];
-var k;
-var numbershigh = [];
-
-for (var i = 0; i < scores.length; i++) {
-    bubblesTest = "Bubble solution #" + i + " score: " + scores[i];
-    if (scores[i] > highestScore) {
-        highestScore = scores[i];
-        mostBiggest = scores[i];
+function printAndGetHighScore(scores) {
+    var bubblesTest;
+    var highestScore = 0;
+    for (var i = 0; i < scores.length; i++) {
+        bubblesTest = "Bubble solution #" + i + " score: " + scores[i];
+        console.log(bubblesTest);
+        if (scores[i] > highestScore) {
+            highestScore = scores[i];
+        }
     }
-    console.log(bubblesTest);
+    return (highestScore)
 }
-
-for (var j = 0; j < scores.length; j++) {
-    if (scores[j] == highestScore) {
-        // numbershigh.push(j);
-    }
-}
-numbershigh = numbershigh + j;
-
-console.log("bubbles tests: " + i)
+var highestScore = printAndGetHighScore(scores);
+console.log("Bubbles tests: " + scores.length);
 console.log("Highest bubble score: " + highestScore);
+
+function getBestResult(scores, highestScore) {
+    var numbershigh = [];
+    for (var j = 0; j < scores.length; j++) {
+        if (scores[j] == highestScore) {
+            numbershigh.push(j);
+        }
+    }
+    return (numbershigh)
+}
+
+var numbershigh = getBestResult(scores, highestScore);
 console.log("Solution with highest score: " + numbershigh);
+
+var costs = [.25, .27, .25, .25, .25, .25,
+    .33, .31, .25, .29, .27, .22,
+    .31, .25, .25, .33, .21, .25,
+    .25, .25, .28, .25, .24, .22,
+    .29, .25, .30, .25, .24, .25,
+    .25, .25, .27, .25, .26, .29
+];
+
+function getMostCosteEffectiveSolution(scores, costs, highestScore) {
+    var cost = 100;
+    var index;
+    for (var i = 0; i < scores.length; i++) {
+        if (scores[i] == highestScore) {
+            if (cost > costs[i]) {
+                index = i;
+                cost = costs[i];
+            }
+        }
+    }
+    return (index);
+}
+var mostCostEffective = getMostCosteEffectiveSolution(scores, costs, highestScore);
+console.log("Bubble Solution #" + mostCostEffective + " is the most cost effective");
